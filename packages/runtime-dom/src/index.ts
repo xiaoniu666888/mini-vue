0ab6140fd0d226fe4731d3c6b64417680a143b01
xiaoniu <270146073@qq.com>
2025-02-09 18:50:28 +0800

    feat: runtime-core开始(1)

--- packages/runtime-dom/src/index.ts
+++ packages/runtime-dom/src/index.ts
@@ -1,20 +1,10 @@
 import { nodeOps } from "./nodeOps";
 import patchProp from "./patchProp";
-
+import { createRenderer } from "@vue/runtime-core"
 
 // 将节点操作和属性操作合并在一起
 const renderOptions = Object.assign({ patchProp }, nodeOps);
-
-// function createRenderer(options: any) {
-
-//     return {
-//         render() {
-//             // patch(null, vnode, container, null, null);
-//         }
-//     }
-// }
-
-// createRenderer(renderOptions).render();
-
 export { renderOptions };
-export * from "@vue/reactivity";
+export * from "@vue/runtime-core";
+
+// 引用关系 runtime-dom -> runtime-core -> reactivity
